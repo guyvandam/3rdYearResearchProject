2cba27f465361dd1f0fa9a705c273ce10d37f083
guyvandam <guyvandam@gmail.com>
2021-06-24 12:16:33 +0300

    LSTM start

--- DataManagement/dataframe.py
+++ DataManagement/dataframe.py
@@ -1,14 +1,15 @@
+from DataManagement.data_manager import DataManager
+from constants import STRING_KEY_NUM_MINUTE_DICT
 import sys
 sys.path.append('./')
 
-from onstants import STRING_KEY_NUM_MINUTE_DICT
-from DataManagement.data_manager import DataManager
 
 class DataFrame():
 
     def __init__(self, coin_sybmol, kline_size_string):
 
-        self.df = DataManager().get_historical_data_DataFrame(coin_sybmol, kline_size_string)
+        self.df = DataManager().get_historical_data_DataFrame(
+            coin_sybmol, kline_size_string)
         self.coin_sybmol = coin_sybmol
         self.kline_size_string = kline_size_string
         self.num_minutes = STRING_KEY_NUM_MINUTE_DICT[self.kline_size_string]
