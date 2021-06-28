c8475678df815fa02c1b52827fb078a1b9751176
guyvandam <guyvandam@gmail.com>
2021-06-28 12:40:11 +0300

    CoinData class.

--- DataManagement/data_manager.py
+++ DataManagement/data_manager.py
@@ -1,22 +1,18 @@
+from DataManagement.dataframe import CoinData
+from DataManagement.pre_processing_functions import PreProcess
+from dateutil import parser
+from datetime import datetime
+from binance.client import Client
+import os.path
+import math
+import pandas as pd
+from constants import STRING_KEY_NUM_MINUTE_DICT, CLOSE_COLUMN, VOLUME_COLUMN
+from DataManagement.file_path_manager import FilePathManager
+from constants import (STRING_KEY_NUM_MINUTE_DICT, TIMESTAMP_COLUMN,
+                       TIMESTAMP_FORMAT)
 import os
 import sys
 sys.path.append('./')
-
-
-from constants import (STRING_KEY_NUM_MINUTE_DICT, TIMESTAMP_COLUMN,
-                       TIMESTAMP_FORMAT)
-
-from DataManagement.file_path_manager import FilePathManager
-
-from constants import STRING_KEY_NUM_MINUTE_DICT, CLOSE_COLUMN, VOLUME_COLUMN
-import pandas as pd
-import math
-import os.path
-from binance.client import Client
-from datetime import datetime
-from dateutil import parser
-
-from DataManagement.pre_processing_functions import PreProcess
 
 
 class DataManager:
@@ -30,11 +26,14 @@ class DataManager:
 
         self.binance_client = Client(api_key=API_KEY, api_secret=API_SECRET)
 
+    def get_historical_data_CoinData(self, symbol: str, kline_size: str) -> CoinData:
+        return CoinData(self.get_historical_data_DataFrame(symbol, kline_size), symbol, kline_size_string=kline_size)
 
     def get_historical_data_DataFrame(self, symbol, kline_size) -> pd.DataFrame:
         symbol = symbol.upper()
         self.__is_possible_kline_size(kline_size)
-        file_path = self.file_path_manager_obj.get_historical_data_csv_file_path(symbol, kline_size)
+        file_path = self.file_path_manager_obj.get_historical_data_csv_file_path(
+            symbol, kline_size)
 
         if os.path.isfile(file_path):
             df = pd.read_csv(file_path)
@@ -44,79 +43,95 @@ class DataManager:
         self.__set_timestamp_index(df)
 
         return df
- 
+
     def __set_timestamp_index(self, df):
-        df[TIMESTAMP_COLUMN] = pd.to_datetime(df[TIMESTAMP_COLUMN], format=TIMESTAMP_FORMAT)
+        df[TIMESTAMP_COLUMN] = pd.to_datetime(
+            df[TIMESTAMP_COLUMN], format=TIMESTAMP_FORMAT)
         df.set_index(TIMESTAMP_COLUMN, inplace=True)
 
     def __minutes_of_new_data(self, symbol, kline_size, data, source):
-        if len(data) > 0:  old = parser.parse(data["timestamp"].iloc[-1])
-        elif source == "binance": old = datetime.strptime('1 Jan 2017', '%d %b %Y')
-        if source == "binance": new = pd.to_datetime(self.binance_client.get_klines(symbol=symbol, interval=kline_size)[-1][0], unit='ms')
+        if len(data) > 0:
+            old = parser.parse(data["timestamp"].iloc[-1])
+        elif source == "binance":
+            old = datetime.strptime('1 Jan 2017', '%d %b %Y')
+        if source == "binance":
+            new = pd.to_datetime(self.binance_client.get_klines(
+                symbol=symbol, interval=kline_size)[-1][0], unit='ms')
         return old, new
 
-    def __get_all_binance(self, symbol, kline_size, save = False):
-        file_path = self.file_path_manager_obj.get_historical_data_csv_file_path(symbol, kline_size)
-        if os.path.isfile(file_path): 
+    def __get_all_binance(self, symbol, kline_size, save=False):
+        file_path = self.file_path_manager_obj.get_historical_data_csv_file_path(
+            symbol, kline_size)
+        if os.path.isfile(file_path):
             data_df = pd.read_csv(file_path)
-        else: 
+        else:
             data_df = pd.DataFrame()
-        oldest_point, newest_point = self.__minutes_of_new_data(symbol, kline_size, data_df, source = "binance")
+        oldest_point, newest_point = self.__minutes_of_new_data(
+            symbol, kline_size, data_df, source="binance")
         delta_min = (newest_point - oldest_point).total_seconds()/60
-        available_data = math.ceil(delta_min/STRING_KEY_NUM_MINUTE_DICT[kline_size])
+        available_data = math.ceil(
+            delta_min/STRING_KEY_NUM_MINUTE_DICT[kline_size])
 
-        if oldest_point == datetime.strptime('1 Jan 2017', '%d %b %Y'): 
-            print('Downloading all available %s data for %s. Be patient..!' % (kline_size, symbol))
-        else: 
-            print('Downloading %d minutes of new data available for %s, i.e. %d instances of %s data.' % (delta_min, symbol, available_data, kline_size))
-        
-        klines = self.binance_client.get_historical_klines(symbol, kline_size, oldest_point.strftime("%d %b %Y %H:%M:%S"), newest_point.strftime("%d %b %Y %H:%M:%S"))
-        data = pd.DataFrame(klines, columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
+        if oldest_point == datetime.strptime('1 Jan 2017', '%d %b %Y'):
+            print('Downloading all available %s data for %s. Be patient..!' %
+                  (kline_size, symbol))
+        else:
+            print('Downloading %d minutes of new data available for %s, i.e. %d instances of %s data.' % (
+                delta_min, symbol, available_data, kline_size))
+
+        klines = self.binance_client.get_historical_klines(symbol, kline_size, oldest_point.strftime(
+            "%d %b %Y %H:%M:%S"), newest_point.strftime("%d %b %Y %H:%M:%S"))
+        data = pd.DataFrame(klines, columns=['timestamp', 'open', 'high', 'low', 'close',
+                            'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
         data['timestamp'] = pd.to_datetime(data['timestamp'], unit='ms')
-        
 
         if len(data_df) > 0:
             temp_df = pd.DataFrame(data)
             data_df = data_df.append(temp_df)
-        else: 
+        else:
             data_df = data
-        
+
         self.__set_timestamp_index(data_df)
 
-        ############################# delete uneccery columns
-        columns_to_delete = ['close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore']            
+        # delete uneccery columns
+        columns_to_delete = ['close_time', 'quote_av',
+                             'trades', 'tb_base_av', 'tb_quote_av', 'ignore']
         for column in data_df.columns:
             if column in columns_to_delete:
                 del data_df[column]
             else:
-                data_df[column] = pd.to_numeric(data_df[column], errors='coerce')
-        
-        ############################### add columns
+                data_df[column] = pd.to_numeric(
+                    data_df[column], errors='coerce')
+
+        # add columns
         self.pre_process_obj.add_precent_change_column(data_df, CLOSE_COLUMN)
         self.pre_process_obj.add_precent_change_column(data_df, VOLUME_COLUMN)
         self.pre_process_obj.add_atr_column(data_df)
         self.pre_process_obj.add_atr_abnormality_column(data_df)
         if kline_size in ['1h', '4h', '1d']:
             one_minute_df = self.get_historical_data_DataFrame(symbol, '1m')
-            self.pre_process_obj.add_high_to_low_precent_change_column(data_df, kline_size, one_minute_df)
-        
+            self.pre_process_obj.add_high_to_low_precent_change_column(
+                data_df, kline_size, one_minute_df)
+
         if save:
             data_df.to_csv(file_path)
         print('All caught up..!')
         return data_df
 
-    def download_historical_data(self, time_frames : list = None, coin_pairs : list = None):
-        time_frames = ["1m", "5m", "15m", "1h", "4h", "1d"] if time_frames is None else time_frames
-        coin_pairs = ["BTCUSDT", "ETHUSDT", "LTCUSDT", "ADAUSDT"] if coin_pairs is None else coin_pairs
+    def download_historical_data(self, time_frames: list = None, coin_pairs: list = None):
+        time_frames = ["1m", "5m", "15m", "1h", "4h",
+                       "1d"] if time_frames is None else time_frames
+        coin_pairs = ["BTCUSDT", "ETHUSDT", "LTCUSDT",
+                      "ADAUSDT"] if coin_pairs is None else coin_pairs
         for time_frame in time_frames:
             for coin_pair in coin_pairs:
                 self.__get_all_binance(coin_pair, time_frame, save=True)
-
 
     def __is_possible_kline_size(self, kline_size):
         if not kline_size in STRING_KEY_NUM_MINUTE_DICT.keys():
             print('not a possible kline size')
             exit()
+
 
 if __name__ == "__main__":
     dm = DataManager()
--- DataManagement/dataframe.py
+++ DataManagement/dataframe.py
@@ -1,15 +1,13 @@
-from DataManagement.data_manager import DataManager
 from constants import STRING_KEY_NUM_MINUTE_DICT
 import sys
 sys.path.append('./')
 
 
-class DataFrame():
+class CoinData():
 
-    def __init__(self, coin_sybmol, kline_size_string):
+    def __init__(self, df, coin_sybmol, kline_size_string):
 
-        self.df = DataManager().get_historical_data_DataFrame(
-            coin_sybmol, kline_size_string)
+        self.df = df
         self.coin_sybmol = coin_sybmol
         self.kline_size_string = kline_size_string
         self.num_minutes = STRING_KEY_NUM_MINUTE_DICT[self.kline_size_string]
