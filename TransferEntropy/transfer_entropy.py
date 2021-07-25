ebb23b21c4d415f221cae4a43872384e691151c3
Tom Bekor <tom.bekor@gmail.com>
2021-07-25 13:36:34 +0300

    TE function fix - numpy array negative index

--- TransferEntropy/transfer_entropy.py
+++ TransferEntropy/transfer_entropy.py
@@ -4,9 +4,10 @@ from TransferEntropy.estimate_entropy_using_copulas import estimateEntropyUsingC
 class TransferEntropy:
 
 
-    def __init__(self, dim: int = 1, lookback: int = 0):
+    def __init__(self, dim: int = 1, lookback: int = 0, use_all_values: bool = True):
         self.dim = dim
         self.lookback = lookback
+        self.use_all_values = use_all_values
     
     def get_correct_array(self,arr: np.array):
         if len(arr.shape) == 1:
@@ -18,8 +19,9 @@ class TransferEntropy:
         # assume the start of each list is the earliest point,  
         # and the end of each list is the latest point.
 
-        arr1 = arr1[-self.lookback:]
-        arr2 = arr2[-self.lookback:]
+        if not self.use_all_values:
+            arr1 = arr1[len(arr1)-self.lookback:]
+            arr2 = arr2[len(arr2)-self.lookback:]
 
         arr1, arr2 = self.get_correct_array(arr1), self.get_correct_array(arr2)
 
@@ -29,8 +31,8 @@ class TransferEntropy:
         #     arr2 = arr2.reshape(len(arr2), 1)
 
         TE = EEC(xs=arr2) - \
-            EEC(xs=arr2[:-1]) - \
-            EEC(xs=np.concatenate((arr2, arr1[:-1]))) + \
-            EEC(xs=np.concatenate((arr2[:-1], arr1[:-1])))
+            EEC(xs=arr2[:len(arr2)-1]) - \
+            EEC(xs=np.concatenate((arr2, arr1[:len(arr1)-1]))) + \
+            EEC(xs=np.concatenate((arr2[:len(arr2)-1], arr1[:len(arr1)-1])))
 
         return TE
