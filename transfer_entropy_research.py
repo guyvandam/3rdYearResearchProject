3a9209729294932d9e16adf28c91a40570f3093d
guyvandam <guyvandam@gmail.com>
2021-08-22 11:51:20 +0300

    .

---
+++ transfer_entropy_research.py
@@ -0,0 +1,18 @@
+
+class TransferEntropy():
+
+
+    pass
+
+class TransferEntropyResearch():
+
+
+    def __init__(self):
+        self.symbols = ["btc", 'eth', 'ltc']
+
+        self.time_frame = ["1h", "1d"]
+
+        self.start_year = ['2018', '2019']
+
+      
+    pass
